0ca0c19eae51a2ba521277f3903e98b798c52599
murat.benli <murat.benli@bilgeadam.com>
2020-06-21 09:58:31 +0300

    Updates

---
+++ precache-manifest.53e59ff93bbe583f000003921319c623.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cb83bfc9cfb616c62a165b6002dbb6",
+    "url": "/WhiteGoodsServices/index.html"
+  },
+  {
+    "revision": "58a28ab82b8dc0814b88",
+    "url": "/WhiteGoodsServices/static/css/main.2e9d3f44.chunk.css"
+  },
+  {
+    "revision": "414c7cdaf06f8eab110f",
+    "url": "/WhiteGoodsServices/static/js/2.155db9cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WhiteGoodsServices/static/js/2.155db9cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58a28ab82b8dc0814b88",
+    "url": "/WhiteGoodsServices/static/js/main.02ac860c.chunk.js"
+  },
+  {
+    "revision": "b79664a39ee950cba349",
+    "url": "/WhiteGoodsServices/static/js/runtime-main.9aee1ba5.js"
+  },
+  {
+    "revision": "ba3f70ef6f513e2b8e98b17166eb0d53",
+    "url": "/WhiteGoodsServices/static/media/airCondition.ba3f70ef.jpg"
+  },
+  {
+    "revision": "3818b97d34b0cb9c3d2257ba13eaa690",
+    "url": "/WhiteGoodsServices/static/media/boiler.3818b97d.jpg"
+  },
+  {
+    "revision": "ea1150816cd8ac412076edd533cb72de",
+    "url": "/WhiteGoodsServices/static/media/calorifere.ea115081.jpg"
+  },
+  {
+    "revision": "2963a32daf2bf9ffb3873b04864c0ea5",
+    "url": "/WhiteGoodsServices/static/media/cover.2963a32d.png"
+  },
+  {
+    "revision": "0ae58dbb2e344cadeda6ddc5a636a99f",
+    "url": "/WhiteGoodsServices/static/media/dishwasher.0ae58dbb.jpg"
+  },
+  {
+    "revision": "7b307934507dcfdc0d72370b14e3dab1",
+    "url": "/WhiteGoodsServices/static/media/dishwasher2.7b307934.png"
+  },
+  {
+    "revision": "62bca91ce8bd190cdf7e501bbc64309c",
+    "url": "/WhiteGoodsServices/static/media/fridge.62bca91c.png"
+  },
+  {
+    "revision": "fc86a55addd013a01797986d05450a61",
+    "url": "/WhiteGoodsServices/static/media/oven.fc86a55a.png"
+  },
+  {
+    "revision": "7cdc1713c8114fe560cd70d9d30734bc",
+    "url": "/WhiteGoodsServices/static/media/posterwithrepairman.7cdc1713.jpg"
+  },
+  {
+    "revision": "0053cd34f8a6bc5f6f4ef012f3267905",
+    "url": "/WhiteGoodsServices/static/media/sevicesPoster.0053cd34.jpg"
+  },
+  {
+    "revision": "45d582939556aaf4f39c10c02aa0a9d2",
+    "url": "/WhiteGoodsServices/static/media/washer.45d58293.png"
+  },
+  {
+    "revision": "e3126900c0b10e17ffa6313fc6a6e624",
+    "url": "/WhiteGoodsServices/static/media/washer2.e3126900.png"
+  },
+  {
+    "revision": "c8be02b515636bbc11acce23edb5d40b",
+    "url": "/WhiteGoodsServices/static/media/whatsapp.c8be02b5.svg"
+  },
+  {
+    "revision": "3f08b31069a676807f4d98b706f0d45a",
+    "url": "/WhiteGoodsServices/static/media/whitegoods1.3f08b310.jpg"
+  }
+]);
